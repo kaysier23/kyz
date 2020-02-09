b710ec318960827bf1ec7b9004a917b183d4bbff
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-09 23:05:14 +0800

    Update class.profile.php

--- class.profile.php
+++ class.profile.php
@@ -1,8 +1,9 @@
+    
 <?php
  class profile{
 	public $db;
 
-	public function_construct(){
+	public function __construct(){
 		$this->db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 		if(mysqli_connect_errno()){
 			echo "Database connection error.";
