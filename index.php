682fef8b352f99de588a953f5b66b7af94f87073
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-09 23:02:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ $json = file_get_contents('https://jsssson.herokuapp.com/json.php');
 $data = json_decode($json,true);
 $list = $data['profile'];
 ?>
-<h1> IZ*ONE PROFILE </h1>
+<h1> BTS members </h1>
 <?php
 foreach($list as $value){
     ?>
 <ul>
-        <h2>$Stage_Name</h2>
+            <h2>$Stage_Name</h2>
               <ul>
                   <li>Rank: $Rank</li>
                   <li>Birth_Name: $Birth_Name </li>
