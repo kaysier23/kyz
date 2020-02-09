bd489e328543f92503a3ad3e06d706d8d6dc79fd
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-09 22:46:09 +0800

    Create class.profile.php

---
+++ class.profile.php
@@ -0,0 +1,22 @@
+<?php
+ class profile{
+	public $db;
+
+	public function __construct(){
+		$this->db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		if(mysqli_connect_errno()){
+			echo "Database connection error.";
+			exit;
+		}
+	}
+
+	public function get_profile(){
+		$sql = "SELECT * FROM tblprofile";
+		$result = mysqli_query($this->db,$sql);
+		while($r = mysqli_fetch_assoc($result)){
+			$list[] = $r;
+		}
+		return $list;
+	}
+	
+}
