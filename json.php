80d4a2929c724b74068d80c9bcfb08554982bd7d
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-09 22:49:00 +0800

    Create json.php

---
+++ json.php
@@ -0,0 +1,14 @@
+    
+<?php
+  include 'config.php';
+  include 'class.profile.php';
+
+  header('Content-Type: application/json');
+
+  $profile = new profile();
+  $list=$profile->get_profile();
+
+  echo "{\"profile\":";
+  echo json_encode($list);
+  echo "}";
+?>
